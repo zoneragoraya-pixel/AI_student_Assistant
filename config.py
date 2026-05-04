e36e627a6bf46dd2657c708ee5618b5d0c448575
zonera maqbool <zoneragoraya@gmail.com>
2026-05-04 10:27:22 +0500

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-from pathlib import Path
-from dotenv import load_dotenv
-
-dotenv_path = Path(__file__).with_name('.env')
-load_dotenv(dotenv_path)
-
-GROQ_API_KEY = os.getenv('GROQ_API_KEY')
